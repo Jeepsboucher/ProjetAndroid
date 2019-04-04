c2e830294fcc82b721083314d29ad47f3b5a52e7
B533P-19\533 <jeepsboucher@gmail.com>
2019-04-04 13:46:39 -0400

    Version Ok recyclerView

--- Project/app/src/main/java/com/jpetalice/cba533/projetandroid/Adapter.java
+++ Project/app/src/main/java/com/jpetalice/cba533/projetandroid/Adapter.java
@@ -1,37 +1,59 @@
 package com.jpetalice.cba533.projetandroid;
 
-import android.support.annotation.NonNull;
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
-    private Recipe[] dataSet;
+    private List<Recipe> dataSet;
 
-    public Adapter(Recipe[] dataSet) {
+    public Adapter(List<Recipe> dataSet) {
         this.dataSet = dataSet;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View recipeView = inflater.inflate(R.layout.recycler_layout, parent,false);
+
+        ViewHolder viewHolder = new ViewHolder(recipeView);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        Recipe recipe = dataSet.get(position);
 
+        TextView textViewName = holder.textView_Name;
+        textViewName.setText(recipe.getName());
+        TextView textViewDescr = holder.textView_Descr;
+        textViewDescr.setText(recipe.getDescr());
     }
 
     @Override
     public int getItemCount() {
 
-        return 0;
+        return dataSet.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        public ViewHolder(View itemView) {
+        public TextView textView_Descr;
+        public TextView textView_Name;
+
+        public ViewHolder(View itemView)
+        {
             super(itemView);
+
+            textView_Descr = (TextView) itemView.findViewById(R.id.textView_description);
+            textView_Name = (TextView) itemView.findViewById(R.id.textView_name);
         }
     }
 }
--- Project/app/src/main/java/com/jpetalice/cba533/projetandroid/HomeActivity.java
+++ Project/app/src/main/java/com/jpetalice/cba533/projetandroid/HomeActivity.java
@@ -5,27 +5,43 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
 
-    private Recipe[] dataSet;
+    private List<Recipe> dataSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        dataSet = new ArrayList<>();
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView_recipe);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         DatabaseHelper database = new DatabaseHelper(this);
-
         Recipe recipeAlice = new Recipe("Recette alice", "bonne poutine grasse");
+
         database.addRecipe(recipeAlice);
+        dataSet.add(recipeAlice);
+        dataSet.add(recipeAlice);
+        dataSet.add(recipeAlice);
+        dataSet.add(recipeAlice);
+        dataSet.add(recipeAlice);
+        dataSet.add(recipeAlice);
+        dataSet.add(recipeAlice);
+        dataSet.add(recipeAlice);
+        dataSet.add(recipeAlice);
+        dataSet.add(recipeAlice);
+
         adapter = new Adapter(dataSet);
         recyclerView.setAdapter(adapter);
+
+
     }
 }
