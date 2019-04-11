e338daf676cba7d8e480f4144ea889797411bcf3
B533P-20\533 <alessard97@hotmail.com>
2019-04-11 14:22:44 -0400

    alert dialog photo

--- Project/app/src/main/java/com/jpetalice/cba533/projetandroid/activity/AddRecipeActivity.java
+++ Project/app/src/main/java/com/jpetalice/cba533/projetandroid/activity/AddRecipeActivity.java
@@ -1,12 +1,17 @@
 package com.jpetalice.cba533.projetandroid.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.jpetalice.cba533.projetandroid.R;
 import com.jpetalice.cba533.projetandroid.data.Recipe;
@@ -26,8 +31,14 @@ public class AddRecipeActivity extends AppCompatActivity {
     }
 
     private void SetListeners(){
-        Button btnSave = findViewById(R.id.btnSave);
+        Button btnAddPhoto = findViewById(R.id.btnAddPhoto);
+        btnAddPhoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                OpenPhotoDialog();
+            }
+        });
 
+        Button btnSave = findViewById(R.id.btnSave);
         btnSave.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String txtName = ((EditText) findViewById(R.id.txtName)).getText().toString();
@@ -35,6 +46,48 @@ public class AddRecipeActivity extends AppCompatActivity {
                 AddRecipe(txtName, txtDescr);
             }
         });
+    }
+
+    private void OpenPhotoDialog(){
+
+        new AlertDialog.Builder(this)
+            .setTitle("Ajouter une photo")
+            .setPositiveButton("Appareil photo", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+                }
+            })
+            .setNegativeButton("Galerie", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto , 1);
+                }
+            })
+            .setNeutralButton("Annuler", null)
+            .show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        ImageView imageview = findViewById(R.id.imgRecipe);
+        switch(requestCode) {
+            case 0:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    imageview.setImageURI(selectedImage);
+                }
+
+                break;
+            case 1:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    imageview.setImageURI(selectedImage);
+                }
+                break;
+        }
     }
 
     private void AddRecipe(String name, String descr){
